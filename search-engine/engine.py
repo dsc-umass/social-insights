523ede442562bdc98bdaf21f8752324cf5808a52
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-22 23:17:45 -0500

    POS Tagging

--- search-engine/engine.py
+++ search-engine/engine.py
@@ -6,7 +6,8 @@ import nltk
 def query(query):
     tokens = nltk.word_tokenize(query)
     print(tokens)
-    
+    tagged = nltk.pos_tag(tokens)
+    print(tagged)
     if(len(tokens) == 1):
         return singleWordQuery(query)
 
