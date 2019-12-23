5e483acf4fea233bfbabdf70d0fc024cce57a073
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-23 14:14:11 -0500

    tests

--- search-engine/engine.py
+++ search-engine/engine.py
@@ -44,6 +44,9 @@ def context(query):
     print(properNouns)
     return "done"
 
+context("Michael Jackson was a legandary basketball player who played in New York.")
+
+
 # Method for query suggest on the frontend
 def suggest(query):
     return "did you mean this?"
