a3c2f11009273ebb97c7c7e5367bac8370b273df
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-26 02:24:12 -0500

    edit distance

--- search-engine/suggest.py
+++ search-engine/suggest.py
@@ -1,10 +1,16 @@
 import nltk
+import time
+start_time = time.time()
+
+def checkTime():
+    print("--- %s seconds ---" % (time.time() - start_time))
+
 """
 Suggest will work the following way:
-    Spellcheck 
-        Both have to be as close as the current query:
+    - Spellcheck 
+    - Both have to be as close as the current query:
         Sugges queries that you have searched before
-        Trending search 
+        Trending searchs
 
 """
 def context(query):
@@ -16,7 +22,23 @@ def context(query):
     print(properNouns)
     return "done"
 
-context("Michael Jackson was a legandary basketball player who played in New York.")
 
 def suggest(query):
     return "did you mean this?"
+
+
+def edit_distance(s1, s2):
+    m=len(s1)+1
+    n=len(s2)+1
+
+    tbl = {}
+    for i in range(m): tbl[i,0]=i
+    for j in range(n): tbl[0,j]=j
+    for i in range(1, m):
+        for j in range(1, n):
+            cost = 0 if s1[i-1] == s2[j-1] else 1
+            tbl[i,j] = min(tbl[i, j-1]+1, tbl[i-1, j]+1, tbl[i-1, j-1]+cost)
+
+    return tbl[i,j]
+
+print(edit_distance("Helloworld ahsd", "HalloWorld"))
