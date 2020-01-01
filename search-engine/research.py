a93a86af9483fefcb7c02bf85f688e26ed143051
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-01-01 14:39:43 -0500

    securing APIs with JWT

--- search-engine/research.py
+++ search-engine/research.py
@@ -1,2 +1,12 @@
-import datetime
-print(datetime.datetime.now())
+import jwt
+
+secret = "hello"
+encoded = jwt.encode({'some': 'payload'}, secret, algorithm='HS256')
+
+print(encoded)
+
+encoded_test = encoded
+
+decoded = jwt.decode(encoded_test, secret, algorithms=['HS256'])
+
+print(decoded)
