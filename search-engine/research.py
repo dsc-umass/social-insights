77dc8ad16075df1d0847fb384f29b31b4a469102
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-01-03 00:53:15 -0500

    minor

--- search-engine/research.py
+++ search-engine/research.py
@@ -4,14 +4,16 @@ import json
 with open('secret.json') as json_file:
     secret = json.load(json_file)['secret']
 
-dataJson = "where are the pit stop locations in SF"
+dataJson = {
+    'hello': 'weird'
+}
+
 encoded = jwt.encode(dataJson, secret, algorithm='HS256')
 
 print(encoded)
 
 encoded_test = encoded
 
-secret_test = "hello"
 decoded = jwt.decode(encoded_test, secret, algorithms=['HS256'])
 
 print(decoded)
