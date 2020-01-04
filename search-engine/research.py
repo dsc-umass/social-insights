6dc36426829b45d13717ac4d8bef677bd6e1f2c1
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-01-05 00:40:37 +0530

    nltk frequency

--- search-engine/research.py
+++ search-engine/research.py
@@ -1,20 +1,37 @@
-import jwt
-import json
+# import jwt
+# import json
 
-with open('secret.json') as json_file:
-    secret = json.load(json_file)['secret']
+# with open('secret.json') as json_file:
+#     secret = json.load(json_file)['secret']
 
-dataJson = {
-    'hello': 'weird'
-}
+# dataJson = {
+#     'hello': 'weird'
+# }
 
-encoded = jwt.encode(dataJson, secret, algorithm='HS256')
+# encoded = jwt.encode(dataJson, secret, algorithm='HS256')
 
-print(encoded)
+# print(encoded)
 
-encoded_test = encoded
+# encoded_test = encoded
 
-decoded = jwt.decode(encoded_test, secret, algorithms=['HS256'])
+# decoded = jwt.decode(encoded_test, secret, algorithms=['HS256'])
 
 # print(decoded)
 
+
+text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is pretty good except that."
+
+import nltk
+allWords = nltk.tokenize.word_tokenize(text)
+allWordDist = nltk.FreqDist(w.lower() for w in allWords)
+
+stopwords = nltk.corpus.stopwords.words('english')
+allWordExceptStopDist = nltk.FreqDist(w.lower() for w in allWords if w not in stopwords)   
+
+
+mostCommon = allWordExceptStopDist.most_common(10)
+
+print(mostCommon)
+
+# for word, frequency in allWordDist.most_common(10):
+#     print('%s;%d' % (word, frequency))
