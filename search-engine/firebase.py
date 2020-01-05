e4dc7a54c7797fe81deeee7225d14b42c1a7485e
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-01-05 08:22:57 +0530

    minor

--- search-engine/firebase.py
+++ search-engine/firebase.py
@@ -10,15 +10,17 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 doc_ref = db.collection('trending-searches').document('searches-log')
 
+test = ""
 try:
     doc = doc_ref.get()
     # print('Document data: {}'.format(doc.to_dict()))
     returnJson = doc.to_dict()
-    for key, value in returnJson:
-        print(key)
+    for key in returnJson:
+        test = test + key + " "
 except google.cloud.exceptions.NotFound:
     print('No such document!')
 
+print(test)
 # doc_ref.set({
 #     "hello world": datetime.datetime.now(),
 #     "hello": datetime.datetime.now()
