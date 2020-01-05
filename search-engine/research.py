e27533d8388e2a8d5659147685894e644ea328bf
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-01-05 08:31:12 +0530

    minor

--- search-engine/research.py
+++ search-engine/research.py
@@ -21,22 +21,23 @@
 
 text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is pretty good except that."
 
-import nltk
-allWords = nltk.tokenize.word_tokenize(text)
-allWordDist = nltk.FreqDist(w.lower() for w in allWords)
+# import nltk
+# allWords = nltk.tokenize.word_tokenize(text)
+# allWordDist = nltk.FreqDist(w.lower() for w in allWords)
 
-stopwords = nltk.corpus.stopwords.words('english')
-allWordExceptStopDist = nltk.FreqDist(w.lower() for w in allWords if w not in stopwords)   
+# stopwords = nltk.corpus.stopwords.words('english')
+# allWordExceptStopDist = nltk.FreqDist(w.lower() for w in allWords if w not in stopwords)   
 
 
-mostCommon = allWordExceptStopDist.most_common(10)
+# mostCommon = allWordExceptStopDist.most_common(10)
 
-mostCommonQuery = []
-for word, frequency in mostCommon:
-    if word != '.' and word != ',':
-        mostCommonQuery.append(word)
+# mostCommonQuery = []
+# for word, frequency in mostCommon:
+#     if word != '.' and word != ',':
+#         mostCommonQuery.append(word)
 
-print(mostCommonQuery)
+# print(mostCommonQuery)
 
 # for word, frequency in allWordDist.most_common(10):
 #     print('%s;%d' % (word, frequency))
+
