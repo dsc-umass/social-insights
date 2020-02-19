a208645de5e389891c1487b0833f9730baf5f1fc
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-18 19:48:55 -0500

    added sample text with search with NLTK

--- search-engine/search.py
+++ search-engine/search.py
@@ -1,18 +1,19 @@
 from whoosh.index import create_in
 from whoosh.fields import *
-schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
-ix = create_in("indexdir", schema)
-writer = ix.writer()
-writer.add_document(title=u"First document", path=u"/a",
-                    content=u"This is the first document we've added!")
-writer.add_document(title=u"Second document", path=u"/b",
-                    content=u"The second one is even more interesting!")
-writer.commit()
-from whoosh.qparser import QueryParser
-with ix.searcher() as searcher:
-    query = QueryParser("content", ix.schema).parse("first")
-    results = searcher.search(query)
-    results[0]
+def whoosh():
+    schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+    ix = create_in("indexdir", schema)
+    writer = ix.writer()
+    writer.add_document(title=u"First document", path=u"/a",
+                        content=u"This is the first document we've added!")
+    writer.add_document(title=u"Second document", path=u"/b",
+                        content=u"The second one is even more interesting!")
+    writer.commit()
+    from whoosh.qparser import QueryParser
+    with ix.searcher() as searcher:
+        query = QueryParser("content", ix.schema).parse("first")
+        results = searcher.search(query)
+        results[0]
 
 
 import nltk
@@ -21,6 +22,7 @@ def nltk_search():
     file = open('NLTK.txt', 'r')
     read_file = file.read()
     text = nltk.Text(nltk.word_tokenize(read_file))
-    match = text.concordance('language')
+    match = text.concordance('of')
+    print(match)
 
 nltk_search()
