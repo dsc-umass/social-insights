a1bc9e008fb38c3145f82d0a01f8cc730683adc4
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-10-01 02:01:08 -0400

    removed react code

--- web/src/components/Database.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-
-let database = ['Pikachu', 'Not Pikachu', 'Arceus', 'Mimikyu', 'Greninja', 'Eevee', 'Not a pokemon', 'Tired', 'Random', 'Some numbers: 1 2 3', '...'];
-
-export {database};
